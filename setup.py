0ccf589d6b439a8e8a6167261c1db4dad573f584
Morugetsuyo <82621702+Morugetsuyo@users.noreply.github.com>
2024-04-07 17:53:17 +0900

    Add dependencies for altgraph, macholib, modulegraph, and py2app

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'packages': [],
+    'plist': {
+        'CFBundleIconFile': 'icon(mac).icns',
+        'CFBundleIdentifier': 'com.example.Autoclicker',
+        'CFBundleName': 'AutoClicker',
+        'CFBundleVersion': '1.0.0',
+        'CFBundleShortVersionString': '1.0'
+    },
+    'iconfile': 'resources/appicon.icns',  
 }
 
 setup(
